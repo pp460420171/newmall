bf1d574aa1ef5cbf3d4b85b94b30888d6c3f519f
unknown <lyc_njust@163.com>
2020-03-20 17:41:03 +0800

    20200320

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <h2>首页</h2>
+    <img src="~assets/img/other/mengmeng.jpg" alt="超可爱"/>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data() { 
-      return {
-        
-      }
-    }
-   }
+export default {
+  name: "Home",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style  scoped>
-  
+img{
+  width: 100%;
+}
 </style>
